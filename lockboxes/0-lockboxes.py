46a2ea40da8e59a10efdac07834139c82f607b42
MOUKIANA-jordy <jordymoukiana@gmail.com>
2025-04-06 19:34:43 +0200

    add two files

---
+++ lockboxes/0-lockboxes.py
@@ -0,0 +1,25 @@
+def canUnlockAll(boxes):
+    """
+    Determines if all boxes can be opened.
+
+    Args:
+        boxes (list of lists): A list where each index represents a box,
+        and each element is a list of keys found in that box.
+
+    Returns:
+        bool: True if all boxes can be opened, False otherwise.
+    """
+    if not boxes or not isinstance(boxes, list):
+        return False
+
+    n = len(boxes)
+    opened = set([0])
+    keys = set(boxes[0])
+
+    while keys:
+        key = keys.pop()
+        if key < n and key not in opened:
+            opened.add(key)
+            keys.update(boxes[key])
+
+    return len(opened) == n
