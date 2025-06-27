e075f4460d5549f870385ddf7f4a63887aba1eea
MOUKIANA-jordy <jordymoukiana@gmail.com>
2025-06-27 18:17:16 +0200

    modification

--- sorted_array_to_avl/binary_trees.h
+++ sorted_array_to_avl/binary_trees.h
@@ -1,25 +1,31 @@
-#ifndef _BINARY_TREES_H_
-#define _BINARY_TREES_H_
+#ifndef BINARY_TREES_H
+#define BINARY_TREES_H
 
+#include <stdlib.h>
 #include <stddef.h>
+#include <stdio.h>
 
 /**
- * struct binary_tree_s - Binary tree node
+ * struct binary_tree_s - Node of a binary tree
  *
  * @n: Integer stored in the node
  * @parent: Pointer to the parent node
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
  */
-typedef struct binary_tree_s
+struct binary_tree_s
 {
-	int n;
-	struct binary_tree_s *parent;
-	struct binary_tree_s *left;
-	struct binary_tree_s *right;
-} binary_tree_t;
+    int n;
+    struct binary_tree_s *parent;
+    struct binary_tree_s *left;
+    struct binary_tree_s *right;
+};
 
-void binary_tree_print(const binary_tree_t *);
+typedef struct binary_tree_s binary_tree_t;
+typedef struct binary_tree_s avl_t;
+
+/* Function prototype */
 avl_t *sorted_array_to_avl(int *array, size_t size);
+void binary_tree_print(const binary_tree_t *tree);
 
-#endif /* _BINARY_TREES_H_ */
+#endif /* BINARY_TREES_H */
